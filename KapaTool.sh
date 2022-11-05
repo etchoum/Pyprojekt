9d2c0bd2d1d5cef867e7cfa2e3ae786d09795680
etchoum <e.tchoumkeungatat@stud.uni-goettingen.de>
2022-11-05 18:34:20 +0000

    Authomated version

---
+++ KapaTool.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# welcome me and install manging dependencies
+echo "Thank you for choosing automated mechanisms, OneJoon GmbH"
+sudo apt update
+sudo apt install git
+git init
+git clone https://github.com/etchoum/Pyprojekt.git
+
+# enter the cloned github repository
+cd Pyprojekt
+sudo apt install python3.9
+python3.9 -m pip install tabulate
+python3.9 -m pip install pandas
+python3.9 model.py
+python3.9 pyscript.py
+
