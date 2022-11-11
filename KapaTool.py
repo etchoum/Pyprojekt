60face77b34388a58340737832d4e270a1afb20c
Live session user <etchoum>
2022-11-11 09:24:39 +0000

    News 11/11/2022

--- KapaTool.py
+++ KapaTool.py
@@ -5,16 +5,16 @@ import time
 d2= time.strftime ("%B %d, %Y %H:%M")
 print('|{}: \n'.format(d2))
 data = pd.read_excel(r'/home/mint/Documents/Pyprojekt/EXPORT_2022_10_21_ Kapadaten.XLSX')
-data_frame = pd.DataFrame(data, columns=["Fertigungsauftragsmaterial", "PSP-Element", "Auftrag", "Vorgangsbeschr.1",
+data_frame = pd.DataFrame(data, columns=["Fertigungsauftragsmaterial", "PSP-Element", "Auftrag", "Vorgangsbeschr.1", "Spät.Start","Spät.Ende",
         "Arbeitsplatz", "Früh.Start", "Früh.Ende", "KapBearb.-Sollbed. (KEINH)", "KapBearb.-Restbed. (KEINH)" , "KapazitätBezeichnung"])
 data = data_frame[["Fertigungsauftragsmaterial", "Auftrag",
-                "Früh.Start", "Früh.Ende",
+                "Früh.Start", "Spät.Ende",
                 "KapBearb.-Sollbed. (KEINH)", "KapBearb.-Restbed. (KEINH)"]]
 def call_Kapadaten_withauftrag(Auftrag_list, dataframe, param):
     for k in Auftrag_list:
         frame = dataframe.T[[k]].T
-        frame["Rest Bearb_zeit"] = frame["Früh.Ende"] - frame["Früh.Start"]
-        frame["Δ KapBearb. Restbedarf"] = frame["KapBearb.-Sollbed. (KEINH)"] - frame["KapBearb.-Restbed. (KEINH)"] 
+        frame["Rest_Bearb_zeit"] = frame["Spät.Ende"] - frame["Früh.Start"]
+        frame["Δ_KapBearb_Restbedarf"] = frame["KapBearb.-Sollbed. (KEINH)"] - frame["KapBearb.-Restbed. (KEINH)"] 
         frame["Auftrag"] = frame["Auftrag"].astype(int)
         if param == False:
             print(frame)
@@ -25,7 +25,7 @@ def call_Kapadaten_withauftrag(Auftrag_list, dataframe, param):
             sum_frame = frame
             sum_frame["SOLL"] = SOLL
             sum_frame["IST"] = IST
-            print(sum_frame)
+            print(f'{sum_frame} \n')
 parser = argparse.ArgumentParser(
         prog='Kapatool.py',
         usage='%(prog)s [options]',
