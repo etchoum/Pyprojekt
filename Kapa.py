0129316f0d72c3a76dad863c1029c798a46f5d35
etchoum <e.tchoumkeungatat@stud.uni-goettingen.de>
2022-11-21 15:47:08 +0000

    News_21.11.2022

--- Kapa.py
+++ Kapa.py
@@ -1,4 +1,5 @@
 #!/bin/env python 
+######################################Modules    & Data ###############################################
 import argparse
 import pandas as pd
 data = pd.read_excel(r'/home/mint/Documents/Pyprojekt/EXPORT_Kapatool_Vorgänge_2022_11_14.XLSX')
@@ -8,6 +9,8 @@ data = data[["Auftrag", "Kurztext Vorgang", "Systemstatus", "Vorgangsmenge (MEIN
 data1 = pd.read_excel(r'/home/mint/Documents/Pyprojekt/EXPORT_Kapatool_Auftragskopf_2022_11_14.XLSX')
 data1 = pd.DataFrame(data1, columns=["Auftrag", "Kunden Code PSP", "PSP-Element", "Systemstatus"])
 
+
+##########################################Functions    ###########################################
 def Kapa_with_auftrag(Input, data, p):
     if p == True:
         dic_of_auftrag = {AUFTRAG:[] for AUFTRAG in Input}
@@ -17,20 +20,20 @@ def Kapa_with_auftrag(Input, data, p):
             data.drop(["Fr.term.Enddat.Durchf", "Fr.term.St.dat.Durchf"], axis = 1, inplace=True)
             auftrag = data.loc[data.index == elment]
             dic_of_auftrag[elment].append(auftrag)
-            print(dic_of_auftrag[elment][0])
-            print(f'{dic_of_auftrag[elment][0][["Vorgangsmenge (MEINH)", "Rückgemeld. Leist. 1 (ILE01)", "Bearbeitungszeit (BEAZE)", "SOLL_Bearb_zeit"]].sum(axis=0)} \n \n')
+            print(f'\n {dic_of_auftrag[elment][0]} \n')
+            print(f'\n {dic_of_auftrag[elment][0][["Vorgangsmenge (MEINH)", "Rückgemeld. Leist. 1 (ILE01)", "Bearbeitungszeit (BEAZE)", "SOLL_Bearb_zeit"]].sum(axis=0)} \n \n')
 
 
 def Kapa_with_status(Input, data, p):
-    dic = {"ERRÖFNET":[] , "IN_BEARBEITUNG":[] , "FERTIG":[] }
+    dic = {"ZU_PLANEN":[] , "IN_BEARBEITUNG":[] , "ABGESCHLOSSEN":[] }
     dic_of_status = {STATUS:[] for STATUS in data.index}
     if p == False:
         for elment in Input:
             status  = data.loc[data.index == elment]
             dic_of_status[elment].append(status)
         for status in Input:
-            print(dic_of_status[status][0])
-            print(dic_of_status[status][0].sum(axis=0, numeric_only=bool))
+            print(f'\n {dic_of_status[status][0]} \n')
+            print(f'\n {dic_of_status[status][0].sum(axis=0, numeric_only=bool)} \n')
     if p == True:
         list_of_states = [str(STATUS) for STATUS in dic_of_status]
         LIST1 = []
@@ -45,38 +48,50 @@ def Kapa_with_status(Input, data, p):
                 LIST2.append(str(state))
             else:
                 LIST3.append(str(state))
-        dic["FERTIG"] = data.T[LIST1].T
-        dic["ERRÖFNET"] = data.T[LIST2].T
+        dic["ABGESCHLOSSEN"] = data.T[LIST1].T
+        dic["ZU_PLANEN"] = data.T[LIST2].T
         dic["IN_BEARBEITUNG"] = data.T[LIST3].T
-        print(dic[Input])
-        print(dic[str(Input)][["Vorgangsmenge (MEINH)", "Rückgemeld. Leist. 1 (ILE01)", "Bearbeitungszeit (BEAZE)", "SOLL_Bearb_zeit"]].sum(axis=0))
-
+        print(f'\n {dic[Input]} \n ')
+        print(f'\n {dic[str(Input)][["Vorgangsmenge (MEINH)", "Rückgemeld. Leist. 1 (ILE01)", "Bearbeitungszeit (BEAZE)", "SOLL_Bearb_zeit"]].sum(axis=0)} \n ')
 
 
 def Kapa_with_psp(Input, data1, p):
-    #data = data.set_index("Auftrag")
+    lst = []
+    for k in data1.index:
+        if str(k).split('-')[0] == Input:
+            if k in lst:
+                pass
+            else:
+                lst.append(k)
     if p == True:
         psp_list = data1.loc[data1.index == Input]
-        print(psp_list)
+        print(f'\n {psp_list} \n')
     if p == False:
-        lst = []
-        #dic = {PSP:[] for PSP in data.index and str(Input) in PSP}
-        for k in data1.index:
-            if str(k).split('-')[0] == Input:
-                if k in lst:
-                    pass
-                else:
-                    lst.append(k) 
-        print(data1.T[lst].T)
+        print(f' \n {data1.T[lst].T} \n')
+    if p == None:
+        list_of_aufträge = data1.T[lst].T.set_index('Auftrag').index
+        AUFTRÄGE = ()
+        for auftrag in list_of_aufträge:
+            if str(auftrag) in data.set_index('Auftrag').index:
+                AUFTRÄGE = AUFTRÄGE +  (str(auftrag),)
+            else:
+                continue
+        print(f' \n Available order numbers for project {Input} are: {", ".join(AUFTRÄGE)} \n')
+        frame = data.set_index('Auftrag').T[list(AUFTRÄGE)].T
+        frame["SOLL_Bearb_zeit"] = frame["Fr.term.Enddat.Durchf"] - frame["Fr.term.St.dat.Durchf"]
+        print(f'\n {frame} \n')
+        print(f'\n {frame[["Vorgangsmenge (MEINH)", "Rückgemeld. Leist. 1 (ILE01)", "Bearbeitungszeit (BEAZE)", "SOLL_Bearb_zeit"]].sum(axis=0) } \n')
+
+
+#def Kapaplanung(Mitarbeiter_list, Kapazitätsfrist):
     
 
-
-
-
+#################################################PARSER   ##########################################
 parser = argparse.ArgumentParser(prog='Kapa.py', usage='%(prog)s [options]', description='Walltimes based on Auftrag number & IST/SOLL Analysis')
 parser.add_argument('-a', '--auftrag', nargs=1, help="-a, or --auftrag for Walltimes based on Auftragnumber")
 parser.add_argument('-s', '--status', nargs=1, help="-s, or --status for Walltimes based on status")
 parser.add_argument('-p', '--psp', nargs=1, help="-s, or --status for Walltimes baed on PSP-Element")
+parser.add_argument('-d', '--details', action='store_true', help="displays details for saved available PSP-Elemente")
 args = parser.parse_args()
 if args.auftrag:
     rows = args.auftrag[0].split(',')
@@ -95,7 +110,7 @@ if args.status:
     for partition in Input:
         if partition in [STATUS for STATUS in data.index]:
             Kapa_with_status(Input, data, p = False)
-        elif partition in ["ERRÖFNET", "IN_BEARBEITUNG", "FERTIG"]:
+        elif partition in ["ZU_PLANEN", "IN_BEARBEITUNG", "ABGESCHLOSSEN"]:
             Kapa_with_status(str(partition), data, p = True)
 if args.psp:
     Input = args.psp[0].split(',')
@@ -105,4 +120,7 @@ if args.psp:
             Kapa_with_psp(Elment, data1, p = True)
         else:
             Kapa_with_psp(Elment, data1, p = False)
+    if args.details:
+        for Element in Input:
+            Kapa_with_psp(Element, data1, p = None)
 
